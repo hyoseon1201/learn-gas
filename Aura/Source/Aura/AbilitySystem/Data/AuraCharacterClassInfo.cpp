e4cefa058e79f4acf392146af009cf61fc652a1a
Hyoseon <wink2090@naver.com>
2025-07-08 06:40:11 +0900

    feat: Character Class Info

---
+++ Aura/Source/Aura/AbilitySystem/Data/AuraCharacterClassInfo.cpp
@@ -0,0 +1,7 @@
+
+#include "AbilitySystem/Data/AuraCharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UAuraCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
